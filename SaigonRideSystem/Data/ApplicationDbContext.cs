b39886ffee31d3a139b3047824de7df7db88e868
minhdan205 <minhdan9136@gmail.com>
2026-05-04 17:26:06 +0700

    Initial ASP.NET Core MVC project setup

---
+++ SaigonRideSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SaigonRideSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet sẽ thêm sau khi mình có tài liệu Class Diagram/ERD
+        // Ví dụ: public DbSet<Station> Stations { get; set; }
+    }
+}
