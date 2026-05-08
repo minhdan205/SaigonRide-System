610d6b6c63191c327ad51c023e6b9b89bfacb35b
minhdan205 <minhdan9136@gmail.com>
2026-05-08 14:57:11 +0700

    Add Breadcrums feature

---
+++ SaigonRideSystem/Models/BreadcrumbItem.cs
@@ -0,0 +1,11 @@
+﻿namespace SaigonRideSystem.Models
+{
+    public class BreadcrumbItem
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string? Url { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
